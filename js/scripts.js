fd50d448d546cb520a3c815422c1c09ecfad75c6
Patrick Lee <patricklee1138@live.com>
2021-03-23 15:34:08 -0700

    add further functionality
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,9 @@ $(document).ready(function() {
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#user").css('background-color', 'teal');
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#webpage").css('background-color', 'teal');
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
     });
@@ -27,6 +29,17 @@ $(document).ready(function() {
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#bonjour").click(function() {
+    $("ul#user").prepend("<li>Sur le pond d'Avignon...</li>");
+    $("ul#webpage").prepend("<li>...on y danse, tout en ronde!</li>");
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
     });
