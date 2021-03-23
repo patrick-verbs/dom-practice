50399e4fb6c161cf82bf3af4a6fab81f4246cca5
Patrick Lee <patricklee1138@live.com>
2021-03-23 15:15:32 -0700

    add message print & delete functionality
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $('li').css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      alert('hi, this message will be removed');
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi, this message will also be removed');
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
